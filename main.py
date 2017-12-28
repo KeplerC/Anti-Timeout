6f111f0bfa034bde49a08c8df44e0cb453c988ae
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2017-12-28 05:09:20 -0800

    itdoescongest

--- main.py
+++ main.py
@@ -2,21 +2,26 @@
 import time
 import os
 import datetime
+import subprocess
+
 def process():
-    time_stamp = "{} : {}\n".format(os.getpid(), str(datetime.datetime.now()))
+    time_stamp = "{} : {}".format(os.getpid(), str(datetime.datetime.now()))
+    print(time_stamp)
     with open("./log", "a") as log:
-        log.write(time_stamp)
+        log.write(time_stamp + "\n")
         log.close()
 
 def main():
     counter = 0
     while True:
         process()
-        time.sleep(3600)
+        time.sleep(2)
         counter += 1
-        if(time == 4):
-            os.fork()
-
+        if(counter == 4):
+            child =subprocess.call(['./script.sh'])
+            print(child)
+            if(child == 0):
+                time.sleep(1)
 
 if __name__ == "__main__":
     main()
